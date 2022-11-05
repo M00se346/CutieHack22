bbe4d1e75914770afd6b17d0f9c360f8403ea09c
Kelsey Musolf <kelseymusolf@ucr-secure-29-10-13-167-75.wnet.ucr.edu>
2022-11-05 09:47:18 -0700

    finished base of main, pusjing for functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,50 @@
 //code here :)
 #include <string>
 #include <iostream>
-using namespace std
+using namespace std;
+
+
+int main(){
+
+    string userInput;
+
+
+    cout << "Hello and welcome to recycle world! Here we help you to recycle the correct way." 
+         << "Please answer these questions and we can tell you were to put your ~trash~ " << endl;
+    
+    cout << "Question 1. are you holding a piece of plastic? Y/N" << endl;
+    cin >> userInput;
+
+    if (userInput == "Y" || userInput == "y"){
+        cout << "great start! Ok, lets look further" << endl;
+        //call plastic function
+    }
+    else{
+        cout << "Ok, lets see. Are you holding a. cardboard b. metal c. paper d. liquid"
+        << " e. none of the above" << endl;
+        cin >> userInput;
+
+        if (userInput == "a"){
+            //call cardboard function
+        }
+        else if (userInput == "b"){
+            //call metal function
+        }
+        else if (userInput == "c"){
+            //call paper function
+        }
+        else if (userInput == "d"){
+            //call liquif function
+        }
+        else if (userInput == "e"){
+            //wtf
+        }
+
+
+
+    }
+
+
+    return 0;
+}
+
